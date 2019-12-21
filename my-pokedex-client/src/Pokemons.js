ec76272680e5fdb702a3fe3ba19b804505082f6d
Maxime404 <galissairemaxime24@gmail.com>
2019-12-21 18:06:17 +0100

    WIP

--- my-pokedex-client/src/Pokemons.js
+++ my-pokedex-client/src/Pokemons.js
@@ -2,7 +2,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Pokemons extends React.Component {
-    state = { pokemons: [] }
+    constructor(props) {
+        super(props);
+        this.state = { pokemons: [] }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
 
     componentDidMount() {
         this.fetchListPokemons();
@@ -16,8 +21,39 @@ export default class Pokemons extends React.Component {
         })
 
         const data = await response.json();
-        console.log(data)
-        this.setState({ pokemons: data.pokemons });
+        //console.log(data)
+        this.setState({ pokemons: data.pokemons.sort((a, b) => a.ndex - b.ndex) });
+    }
+
+    handleChange(event) {
+        switch (true) {
+            case (event.target.value == 'orderByNdex'):
+                this.setState({ pokemons: this.state.pokemons.sort((a, b) => a.ndex - b.ndex) });
+                break;
+
+            case (event.target.value == 'disorderByNdex'):
+                this.setState({ pokemons: this.state.pokemons.sort((a, b) => b.ndex - a.ndex) });
+                break;
+
+            case (event.target.value == 'orderByName'):
+                this.setState({
+                    pokemons: this.state.pokemons.sort((a, b) => {
+                        return (a.nom > b.nom) ? 1 : (a.nom < b.nom) ? -1 : 0
+                    })
+                });
+                break;
+
+            case (event.target.value == 'disorderByName'):
+                this.setState({
+                    pokemons: this.state.pokemons.sort((a, b) => {
+                        return (b.nom > a.nom) ? 1 : (b.nom < a.nom) ? -1 : 0;
+                    })
+                });
+                break;
+
+            default:
+                this.setState({ pokemons: this.state.pokemons.sort((a, b) => a.ndex - b.ndex) });
+        }
     }
 
     render() {
@@ -27,17 +63,27 @@ export default class Pokemons extends React.Component {
             <div>
                 <h1>Pokemon list:</h1>
                 <div class="container">
+                    <div>
+                        <label for="orderBy">Trier par :  </label>
+                        <select name="orderBy " type="text" onChange={this.handleChange}>
+                            <option value="orderByNdex">Ndex</option>
+                            <option value="disorderByNdex">- Ndex</option>
+                            <option value="orderByName">Alphabétique</option>
+                            <option value="disorderByName">- Alphabétique</option>
+                        </select>
+                    </div>
                     <div class="row">
-                        {pokemons.map(pokemon => (
-                            <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div class="pokemon-v-card">
-                                    <img src={'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + pokemon.ndex + '.png'} alt={pokemon.ndex} />
-                                    <p key={pokemon.ndex}>
-                                        #{pokemon.ndex} {pokemon.nom}
-                                    </p>
+                        {pokemons
+                            .map(pokemon => (
+                                <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
+                                    <div class="pokemon-v-card">
+                                        <img src={'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + pokemon.ndex + '.png'} alt={pokemon.ndex} />
+                                        <p key={pokemon.ndex}>
+                                            #{pokemon.ndex} {pokemon.nom}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
                     </div>
                 </div>
             </div>
